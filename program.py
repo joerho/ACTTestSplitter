4ad6535d5a9a01b35b06f3878b93afde792eaaab
joerho1@gmail.com <joerho1@gmail.com>
2019-09-05 16:25:06 -0700

    splits test and also mixes sections but needs more work

--- program.py
+++ program.py
@@ -7,11 +7,12 @@ from reportlab.pdfgen import canvas
 import sys
 import os
 import re
+import itertools
 
 #python3 program.py test.pdf cover.pdf [page ranges]
 CONST_OUTDIR = '/Users/joerho/Desktop/pyPDF/ACT/Subject Separated'
 CONST_NUM_SECTIONS = 4
-CONST_SECTION_MAP = {1:'E', 2:'M', 3:'R' , 4:'S'}
+CONST_SECTION_MAP = {0:'E', 1:'M', 2:'R' , 3:'S'}
 CONST_ENGLISH = '/Users/joerho/Desktop/pyPDF/Test Covers/english.png'
 CONST_MATH = '/Users/joerho/Desktop/pyPDF/Test Covers/math.png'
 CONST_READING = '/Users/joerho/Desktop/pyPDF/Test Covers/reading.png'
@@ -28,13 +29,15 @@ def main(argv):
 	pageRanges = parsedArgv[1]
 	testNameNum = getTestName(parsedArgv[0])
 	outputDirs = prepareOutputDirectory(testNameNum[0])
+	print(outputDirs)
 
 	splitPages(testNameNum, testPath, pageRanges, outputDirs)
+	mixAndMatch(testNameNum, testPath, outputDirs)
 
 
 def splitPages(testNameNum, testPath, pageRanges, outputDirs):
 	temp = 1
-	key = 1
+	key = 0
 	for bookmark in pageRanges:
 		f = open(testPath, 'rb')
 		pdf = PdfFileReader(f)
@@ -50,14 +53,92 @@ def splitPages(testNameNum, testPath, pageRanges, outputDirs):
 		for page in range(temp, bookmark):
 			pdfWriter.addPage(pdf.getPage(page))
 
-		outputFileName = os.path.join(outputDirs[1], '{} {} {}.pdf'
-			.format(testNameNum[1], testNameNum[0], CONST_SECTION_MAP[key]))
+		outputFileName = genSectionFilePath(outputDirs[1], testNameNum[1], testNameNum[0], key)
 		with open(outputFileName, 'wb') as out:
 			pdfWriter.write(out)
 		temp = bookmark
 		key += 1
 
+	
 	f.close()
+
+# def mixAndMatch(testNameNum, testPath, outputDirs):
+# 	eachSection = []
+# 	origCover = getOrigCover(testPath)
+
+# 	for key in range(0,CONST_NUM_SECTIONS):
+# 		section = genSectionFilePath(outputDirs[1], testNameNum[1], testNameNum[0], key)
+# 		with open(section, 'rb') as f:
+# 			pdf = PdfFileReader(f)
+# 			eachSection.append(pdf.pages[1:])
+# 	l = [0,1,2,3]
+# 	for L in range(2, len(l)):
+# 		for subset in itertools.combinations(l,L):
+# 			if len(subset) == 2:
+# 				outputFileName = os.path.join(outputDirs[0], '{} {} {}{}.pdf'
+# 				 .format(testNameNum[1], testNameNum[0], CONST_SECTION_MAP[subset[0]], 
+# 				 CONST_SECTION_MAP[subset[1]]))
+# 				print(eachSection)
+# 				pdfConcat(origCover, [eachSection[subset[0]], eachSection[subset[1]]], outputFileName)
+
+def mixAndMatch(testNameNum, testPath, outputDirs):
+	l = [0,1,2,3]
+	for L in range(2,len(l)):
+		for subset in itertools.combinations(l,L):
+			if len(subset) == 2:
+				eachSection = list()
+				outputFileName = os.path.join(outputDirs[0], '{} {} {}{}.pdf'
+				 .format(testNameNum[1], testNameNum[0], CONST_SECTION_MAP[subset[0]], 
+				 CONST_SECTION_MAP[subset[1]]))
+				eachSection.append(genSectionFilePath(outputDirs[1], testNameNum[1], testNameNum[0], subset[0]))
+				eachSection.append(genSectionFilePath(outputDirs[1], testNameNum[1], testNameNum[0], subset[1]))
+				pdfConcat(eachSection, outputFileName, testPath)
+
+			if len(subset) == 3:
+				eachSection = list()
+				outputFileName = os.path.join(outputDirs[0], '{} {} {}{}{}.pdf'
+				 .format(testNameNum[1], testNameNum[0], CONST_SECTION_MAP[subset[0]], 
+				 CONST_SECTION_MAP[subset[1]], CONST_SECTION_MAP[subset[2]]))
+				eachSection.append(genSectionFilePath(outputDirs[1], testNameNum[1], testNameNum[0], subset[0]))
+				eachSection.append(genSectionFilePath(outputDirs[1], testNameNum[1], testNameNum[0], subset[1]))
+				eachSection.append(genSectionFilePath(outputDirs[1], testNameNum[1], testNameNum[0], subset[2]))
+				pdfConcat(eachSection, outputFileName, testPath)
+
+
+
+
+def pdfConcat(inputFiles, outputFileName, testPath):
+	pdfWriter = PdfFileWriter()
+	# with open(testPath, 'rb') as f:
+	# 	pdf = PdfFileReader(f)
+	# 	pdfWriter.addPage(pdf.getPage(0))
+
+	for filePath in inputFiles:
+		f = open(filePath , 'rb')
+		pdfReader = PdfFileReader(f)
+		
+		
+		for page in pdfReader.pages[1:]:
+			pdfWriter.addPage(page)
+	
+
+	with open(outputFileName, 'wb') as out:
+		pdfWriter.write(out)
+
+def addPages(pdfWriter, pdf):
+	for page in pdf:
+		pdfWriter.addPage(page)
+
+def genSectionFilePath(outputDir, testNum, testName, sectionKey):
+	return os.path.join(outputDir, '{} {} {}.pdf'
+	 .format(testNum, testName, CONST_SECTION_MAP[sectionKey]))
+				
+
+def getOrigCover(testPath):
+	with open(testPath, 'rb') as f:
+		pdf = PdfFileReader(f)
+		return pdf.getPage(0)
+
 
 def checkArgv(argv):
 	testPath = argv[0]
@@ -137,25 +218,21 @@ def prepareOutputDirectory(testName):
 
 def addWaterMark(origCover, key):
 	c = canvas.Canvas('watermark.pdf')
-	if key == 1:
+	if key == 0:
 		c.drawImage(CONST_ENGLISH, 190, 220)
-	elif key == 2:
+	elif key == 1:
 		c.drawImage(CONST_MATH, 220, 220)
-	elif key == 3:
+	elif key == 2:
 		c.drawImage(CONST_READING, 190, 220)
-	elif key == 4:
+	elif key == 3:
 		c.drawImage(CONST_SCIENCE, 190, 220)
 
 	c.save()
 	watermark = PdfFileReader(open("watermark.pdf", "rb"))
 	origCover.mergePage(watermark.getPage(0))
 	os.remove("watermark.pdf")
+
 	return origCover
-
-
-
-
-
 
 if __name__ == '__main__':
 	main(sys.argv[1:])
