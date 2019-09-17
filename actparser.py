0918633b53e4c833fcb784f9a1f1e168d30b2d6b
joerho1@gmail.com <joerho1@gmail.com>
2019-09-16 18:40:50 -0700

    changes incoming for uploading

--- actparser.py
+++ actparser.py
@@ -8,82 +8,110 @@ import sys
 import os
 import re
 import itertools
+import dropbox
 
 #python3 program.py test.pdf cover.pdf [page ranges]
+CONST_ACCESS_TOKEN = 'do2GicKOS1AAAAAAAAAC0ARZMyPNvzfbbvA3073XwjYFtErsHsS4ZmCGWkx53YRN'
 CONST_TESTPATH = '/Users/joerho/ModusDropBox/Dropbox/Modus Teachers (Shared)/1 ACT/ REAL ACT TESTS/Single File'
-CONST_OUTDIR = '/Users/joerho/ModusDropBox/Dropbox/Modus Teachers (Shared)/9 Joe/ACT/SubjectSeparated'
+CONST_OUTDIR = '/testparser/ACT/SubjectSeparated'
 CONST_NUM_SECTIONS = 4
 CONST_SECTION_MAP = {0:'E', 1:'M', 2:'R' , 3:'S'}
 CONST_ENGLISH = '/Users/joerho/Desktop/pyPDF/Test Covers/english.png'
 CONST_MATH = '/Users/joerho/Desktop/pyPDF/Test Covers/math.png'
 CONST_READING = '/Users/joerho/Desktop/pyPDF/Test Covers/reading.png'
 CONST_SCIENCE = '/Users/joerho/Desktop/pyPDF/Test Covers/science.png'
+CONST_LOCAL = 'temporaryPDF'
 
+class TransferData:
+    def __init__(self, access_token):
+        self.access_token = access_token
+
+    def upload_file(self, file_from, file_to):
+        dbx = dropbox.Dropbox(self.access_token)
+
+        with open(file_from, 'rb') as f:
+            dbx.files_upload(f.read(), file_to)
 
 def actparse(argv):
 	print("\n\nParsing arguments...")
-	parsedArgv = checkArgv(argv)
-	print(parsedArgv)
+	parsed_argv = check_argv(argv)
+	print(parsed_argv)
 
-	testPath = os.path.join(CONST_TESTPATH, parsedArgv[0])
-	pageRanges = parsedArgv[1]
-	testNameNum = getTestName(testPath)
-	outputDirs = prepareOutputDirectory(testNameNum[0])
+	test_path = os.path.join(CONST_TESTPATH, parsed_argv[0])
+	page_ranges = parsed_argv[1]
+	testnamenum = get_testnamenum(test_path)
+	output_dirs = prepare_output_dir(testnamenum[0])
 
-	splitPages(testNameNum, testPath, pageRanges, outputDirs)
-	mixAndMatch(testNameNum, testPath, outputDirs)
+	split_pages(testnamenum, test_path, page_ranges, output_dirs)
+	mix_match(testnamenum, test_path, output_dirs)
 
-def splitPages(testNameNum, testPath, pageRanges, outputDirs):
+	remove_local_files()
+
+def remove_local_files():
+	filelist = os.listdir(CONST_LOCAL)
+	for f in filelist:
+		os.remove(os.path.join(CONST_LOCAL, f))
+
+def upload_dropbox(local_filename, output_filename):
+	transferData = TransferData(CONST_ACCESS_TOKEN)
+	transferData.upload_file(local_filename, output_filename)
+
+def split_pages(testnamenum, test_path, page_ranges, output_dirs):
 	temp = 1
 	key = 0
-	for bookmark in pageRanges:
-		f = open(testPath, 'rb')
+	for bookmark in page_ranges:
+		f = open(test_path, 'rb')
 		pdf = PdfFileReader(f)
-		pdfWriter = PdfFileWriter()
+		pdf_writer = PdfFileWriter()
 		
 		#add watermark to original cover
-		origCover = pdf.getPage(0)
-		newCover = addWaterMark(origCover, key)
+		orig_cover = pdf.getPage(0)
+		new_cover = add_watermark(orig_cover, key)
 
 		#put new cover on the front
-		pdfWriter.addPage(newCover)
+		pdf_writer.addPage(new_cover)
 
 		for page in range(temp, bookmark):
-			pdfWriter.addPage(pdf.getPage(page))
+			pdf_writer.addPage(pdf.getPage(page))
 
-		outputFileName = genSectionFilePath(outputDirs, testNameNum[1], testNameNum[0], key)
-		with open(outputFileName, 'wb') as out:
-			pdfWriter.write(out)
+		local_filename = generate_section_filepath(CONST_LOCAL, testnamenum, key)
+		output_filename = generate_section_filepath(output_dirs, testnamenum, key)
+
+		with open(local_filename, 'wb') as out:
+			pdf_writer.write(out)
+
+		# upload_dropbox(local_filename, output_filename)
+
 		temp = bookmark
 		key += 1
 
 	f.close()
 
 
-def mixAndMatch(testNameNum, testPath, outputDirs):
-	subsets = generateKeyCombo()
+def mix_match(testnamenum, test_path, output_dirs):
+	subsets = generate_keycombo()
 	for subset in subsets:
-		eachSection = list()
+		each_section = list()
 		size = len(subset)
 
 		if size == 2:
-			outputFileName = os.path.join(outputDirs, '{} {} {}{}.pdf'
-			 .format(testNameNum[1], testNameNum[0], CONST_SECTION_MAP[subset[0]], 
-			 CONST_SECTION_MAP[subset[1]]))
-			eachSection.append(genSectionFilePath(outputDirs, testNameNum[1], testNameNum[0], subset[0]))
-			eachSection.append(genSectionFilePath(outputDirs, testNameNum[1], testNameNum[0], subset[1]))
+			output_filename = os.path.join(output_dirs, '{} {} {}{}.pdf'
+				.format(testnamenum[1], testnamenum[0], CONST_SECTION_MAP[subset[0]], 
+			 	CONST_SECTION_MAP[subset[1]]))
+			each_section.append(generate_section_filepath(CONST_LOCAL, testnamenum, subset[0]))
+			each_section.append(generate_section_filepath(CONST_LOCAL, testnamenum, subset[1]))
 
 		if size == 3:
-			outputFileName = os.path.join(outputDirs, '{} {} {}{}{}.pdf'
-			 .format(testNameNum[1], testNameNum[0], CONST_SECTION_MAP[subset[0]], 
-			 CONST_SECTION_MAP[subset[1]], CONST_SECTION_MAP[subset[2]]))
-			eachSection.append(genSectionFilePath(outputDirs, testNameNum[1], testNameNum[0], subset[0]))
-			eachSection.append(genSectionFilePath(outputDirs, testNameNum[1], testNameNum[0], subset[1]))
-			eachSection.append(genSectionFilePath(outputDirs, testNameNum[1], testNameNum[0], subset[2]))
+			output_filename = os.path.join(output_dirs, '{} {} {}{}{}.pdf'
+				.format(testnamenum[1], testnamenum[0], CONST_SECTION_MAP[subset[0]], 
+				CONST_SECTION_MAP[subset[1]], CONST_SECTION_MAP[subset[2]]))
+			each_section.append(generate_section_filepath(CONST_LOCAL, testnamenum, subset[0]))
+			each_section.append(generate_section_filepath(CONST_LOCAL, testnamenum, subset[1]))
+			each_section.append(generate_section_filepath(CONST_LOCAL, testnamenum, subset[2]))
 		
-		pdfConcat(eachSection, outputFileName, testPath)
+		pdfConcat(each_section, output_filename, test_path)
 
-def generateKeyCombo():
+def generate_keycombo():
 	keys = list()
 	subsets = list()
 
@@ -96,87 +124,88 @@ def generateKeyCombo():
 
 	return subsets
 
-def pdfConcat(inputFiles, outputFileName, testPath):
-	pdfWriter = PdfFileWriter()
-	temp = open(testPath, 'rb')
+def pdfConcat(each_section, output_filename, test_path):
+	pdf_writer = PdfFileWriter()
+	temp = open(test_path, 'rb')
 	pdf = PdfFileReader(temp)
-	pdfWriter.addPage(pdf.getPage(0))
+	pdf_writer.addPage(pdf.getPage(0))
 
-	for filePath in inputFiles:
-		f = open(filePath , 'rb')
-		pdfReader = PdfFileReader(f)
+	for file_path in each_section:
+		f = open(file_path , 'rb')
+		pdf_reader = PdfFileReader(f)
 		
-		for page in pdfReader.pages[1:]:
-			pdfWriter.addPage(page)
+		for page in pdf_reader.pages[1:]:
+			pdf_writer.addPage(page)
 
-	with open(outputFileName, 'wb') as out:
-		pdfWriter.write(out)
+	head, tail = os.path.split(output_filename)
+	local_filename = os.path.join(CONST_LOCAL, tail)
+
+	with open(local_filename, 'wb') as out:
+		pdf_writer.write(out)
+		
+	# upload_dropbox(local_filename, output_filename)
 
 	temp.close()
 
-def addPages(pdfWriter, pdf):
-	for page in pdf:
-		pdfWriter.addPage(page)
-
-def genSectionFilePath(outputDir, testNum, testName, sectionKey):
-	return os.path.join(outputDir, '{} {} {}.pdf'
-	 .format(testNum, testName, CONST_SECTION_MAP[sectionKey]))
+def generate_section_filepath(output_dir, testnamenum, sectionKey):
+	return os.path.join(output_dir, '{} {} {}.pdf'
+	 .format(testnamenum[1], testnamenum[0], CONST_SECTION_MAP[sectionKey]))
 				
 
-def getOrigCover(testPath):
-	with open(testPath, 'rb') as f:
+def getOrigCover(test_path):
+	with open(test_path, 'rb') as f:
 		pdf = PdfFileReader(f)
 		return pdf.getPage(0)
 
 
-def checkArgv(argv):
-	testName = argv[0]
-	pageRanges = argv[1]
-	testPath = os.path.join(CONST_TESTPATH, testName)
+def check_argv(argv):
+	test_name = argv[0]
+	page_ranges = argv[1]
+	test_path = os.path.join(CONST_TESTPATH, test_name)
 
 	try:
-		t = open(testPath, 'rb')
+		t = open(test_path, 'rb')
 	except Exception as e:
 		print("The test pdf does not exist.")
 	
 	t.close()
 
-	pageArr = checkPageRanges(pageRanges)
+	page_arr = checkPageRanges(page_ranges)
 
-	return [testPath, pageArr]
+	return [test_path, page_arr]
 
 
-def checkPageRanges(pageRanges):
-	pageArr = pageRanges.split(',')
+def checkPageRanges(page_ranges):
+	page_arr = page_ranges.split(',')
 	try:
-		pageArr = [int(x) for x in pageArr]
+		page_arr = [int(x) for x in page_arr]
 	except:
 		print("Each value of page range must be an int.")
 
-	if isPositive(pageArr) == False:
+	if isPositive(page_arr) == False:
 		print("Why would you put in a negative number..?")
 		sys.exit()
 
-	if isSorted(pageArr) == False:
+	if isSorted(page_arr) == False:
 		print("Page range has to be sorted.")
 		sys.exit()
 
-	if len(pageArr) != CONST_NUM_SECTIONS:
+	if len(page_arr) != CONST_NUM_SECTIONS:
 		print("Page range must be a length of {}".format(CONST_NUM_SECTIONS))
 		sys.exit()
 
-	return pageArr
+	return page_arr
 
-def isPositive(pageArr):
-	for x in pageArr:
+def isPositive(page_arr):
+	for x in page_arr:
 		if x <= 0:
 			return False
 
 	return True
 
-def isSorted(pageArr):
-	temp = pageArr[0]
-	for x in pageArr[1:]:
+def isSorted(page_arr):
+	temp = page_arr[0]
+	for x in page_arr[1:]:
 		if x < temp:
 			return False
 		else:
@@ -184,31 +213,31 @@ def isSorted(pageArr):
 
 	return True
 
-def getTestName(testPath):
-	path, testName = os.path.split(testPath)
-	arr = testName.split(" ")
-	testNum = re.search('\(([^)]+)', arr[-1]).group(1)
-	arr = [''.join(arr[:-1]), testNum]
+def get_testnamenum(test_path):
+	path, test_name = os.path.split(test_path)
+	arr = test_name.split(" ")
+	test_num = re.search('\(([^)]+)', arr[-1]).group(1)
+	arr = [''.join(arr[:-1]), test_num]
 
 	return arr
 
-def prepareOutputDirectory(testName):
-	if not os.path.exists(CONST_OUTDIR):
-		os.makedirs(CONST_OUTDIR)
+def prepare_output_dir(test_name):
+	# if not os.path.exists(CONST_OUTDIR):
+	# 	os.makedirs(CONST_OUTDIR)
 
-	testDirPath = os.path.join(CONST_OUTDIR, testName)
-	if not os.path.exists(testDirPath):
-		os.makedirs(testDirPath)
+	test_directory = os.path.join(CONST_OUTDIR, test_name)
+	# if not os.path.exists(test_directory):
+	# 	os.makedirs(test_directory)
 
-	return testDirPath
+	return test_directory
 
-	# individualPath = os.path.join(testDirPath, "Individual Sections")
+	# individualPath = os.path.join(test_directory, "Individual Sections")
 	# if not os.path.exists(individualPath):
 	# 	os.makedirs(individualPath)
 
-	# return [testDirPath, individualPath]
+	# return [test_directory, individualPath]
 
-def addWaterMark(origCover, key):
+def add_watermark(orig_cover, key):
 	c = canvas.Canvas('watermark.pdf')
 	if key == 0:
 		c.drawImage(CONST_ENGLISH, 190, 220)
@@ -221,10 +250,10 @@ def addWaterMark(origCover, key):
 
 	c.save()
 	watermark = PdfFileReader(open("watermark.pdf", "rb"))
-	origCover.mergePage(watermark.getPage(0))
+	orig_cover.mergePage(watermark.getPage(0))
 	os.remove("watermark.pdf")
 
-	return origCover
+	return orig_cover
 
 # if __name__ == '__main__':
 # 	main(sys.argv[1:])
